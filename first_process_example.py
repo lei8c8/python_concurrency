f350532872f7f89db1894ea2afee84a1ad5234e3
Lei Chen <lei8c8@gmail.com>
2020-12-28 13:26:52 -0500

    first process example

---
+++ first_process_example.py
@@ -0,0 +1,17 @@
+from multiprocessing import Process
+from multiprocessing import current_process
+import os
+
+def process_task():
+    print("{0} has pid: {1} with parent pid: {2}".format(current_process().name, os.getpid(), os.getppid()))
+
+if __name__ == '__main__':
+    process = [None] * 3
+    for i in range(3):
+        process[i] = Process(target=process_task, name="process-{0}".format(i))
+        process[i].start()
+
+    for i in range(3):
+        process[i].join()
+
+    print("{0} has pid {1}".format(current_process().name, os.getpid()))
