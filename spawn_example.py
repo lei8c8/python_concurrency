eeb928fe39f39047b570e95c29a4620ba67225ef
Lei Chen <lei8c8@gmail.com>
2020-12-28 14:11:03 -0500

    spawn example

---
+++ spawn_example.py
@@ -0,0 +1,20 @@
+from multiprocessing import Process
+import multiprocessing
+
+class Test:
+    value = 777
+
+
+def process_task():
+    print(Test.value)
+
+
+if __name__ == '__main__':
+    multiprocessing.set_start_method('spawn')
+
+    # change the value of Test.value before creating
+    # a new process
+    Test.value = 999
+    process = Process(target=process_task, name="process-1")
+    process.start()
+    process.join()
